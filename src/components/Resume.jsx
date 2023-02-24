36e90170c66f70504121ed43b0f1b8b70ce1e8c8
WitchingHr <matt1494@gmail.com>
2023-02-24 15:55:17 -0500

    Add conditional rendering for resume categories

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -15,59 +15,72 @@ export default function Resume({ data }) {
 
       <div className="left-flex">
         <ResumeContact />
-        <div>
-          <div className="category">Education</div>
-          <hr className="hr"></hr>
-          <div className="type-wrapper">
-            {data[1].children.map((child) =>
-              <ResumeEducation key={child.id} id={child.id} data={data[1].children} />
-            )}
+
+        {data[1].children.length > 0 && (
+          <div>
+              <div className="category">Education</div>
+              <hr className="hr"></hr>
+              <div className="type-wrapper">
+                {data[1].children.map((child) =>
+                  <ResumeEducation key={child.id} id={child.id} data={data[1].children} />
+                )}
+              </div>
           </div>
-        </div>
-        <div>
-          <div className="category">Expertise</div>
-          <hr className="hr"></hr>
-          <ul className="type-wrapper skills-ul">
-            {data[3].children.map((child) =>
-              <ResumeSkills key={child.id} id={child.id} data={data[3].children} />
-            )}
-          </ul>
-        </div>
+        )}
+
+        {data[3].children.length > 0 && (
+          <div>
+            <div className="category">Expertise</div>
+            <hr className="hr"></hr>
+            <ul className="type-wrapper skills-ul">
+              {data[3].children.map((child) =>
+                <ResumeSkills key={child.id} id={child.id} data={data[3].children} />
+              )}
+            </ul>
+          </div>
+        )}
       </div>
 
-      <div className="languages">
-        <div className="category">Languages</div>
-        <hr className="hr"></hr>
-          <ul className="languages-ul">
-            {data[4].children.map((child) =>
-              <ResumeLanguages key={child.id} id={child.id} data={data[4].children} />
-            )}
-          </ul>
-      </div>
+      {data[4].children.length > 0 && (
+        <div className="languages">
+          <div className="category">Languages</div>
+          <hr className="hr"></hr>
+            <ul className="languages-ul">
+              {data[4].children.map((child) =>
+                <ResumeLanguages key={child.id} id={child.id} data={data[4].children} />
+              )}
+            </ul>
+        </div>
+      )}
 
       <ResumeIdentity />
 
-      <div className="right-flex resume-right">
-        <div>
-          <div className="category-invert">Experience</div>
-          <hr className="hr-invert"></hr>
-          <div className="type-wrapper">
-            {data[2].children.map((child) =>
-              <ResumeExperience key={child.id} id={child.id} data={data[2].children} />
-            )}
-
+      <div className="resume-right">
+        {data[2].children.length > 0 && (
+          <div>
+            <div className="category-invert">Experience</div>
+            <hr className="hr-invert"></hr>
+            <div className="type-wrapper">
+              {data[2].children.map((child) =>
+                <ResumeExperience key={child.id} id={child.id} data={data[2].children} />
+              )}
+            </div>
           </div>
-        </div>
+        )}
       </div>
 
-      <div className="references resume-right">
-        <div className="category-invert">References</div>
-        <hr className="hr-invert"></hr>
-        <div className="ref-wrapper">
-          {data[5].children.map((child) =>
-            <ResumeReferences key={child.id} id={child.id} data={data[5].children} />
-          )}
-        </div>
+      <div className="resume-right">
+        {data[5].children.length > 0 && (
+          <div className="references">
+            <div className="category-invert">References</div>
+            <hr className="hr-invert"></hr>
+            <div className="ref-wrapper">
+              {data[5].children.map((child) =>
+                <ResumeReferences key={child.id} id={child.id} data={data[5].children} />
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
